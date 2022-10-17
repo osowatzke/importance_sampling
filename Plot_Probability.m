3f5d12016691e703611beffa42fb995071c2b0d8
Owen <osowatzke@gmail.com>
2022-10-16 18:23:25 -0700

    Continued Explanation of Important Sampling

--- Plot_Probability.m
+++ Plot_Probability.m
@@ -15,3 +15,22 @@ grid on;
 xlabel('X');
 ylabel('f_X(x)')
 title('Computing P\{X > 3.957\} from f_X(x)');
+
+u = 3.957;
+sigma = 1;
+T = 3.957;
+x = 0:0.001:8;
+fx = 1/sqrt(2*pi*sigma^2)*exp(-(x-u).^2/(2*sigma^2));
+figure(2);
+clf;
+plot(x, fx)
+hold on;
+fx = fx(x >= 3.957);
+x = x(x >= 3.957);
+area(x, fx);
+legend('g_X(x)','Important Samples')
+grid on;
+xlabel('X');
+ylabel('g_X(x)')
+title('Important Samples from g_X(x)');
+
